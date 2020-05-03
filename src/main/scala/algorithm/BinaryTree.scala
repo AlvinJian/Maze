d6f1a2714bee64aa307a63df8acedd127c5fae32
Alvin Chien <alvinchien0624@gmail.com>
2020-05-03 19:22:53 -0400

    add Sidewinder algorithm

--- src/main/scala/algorithm/BinaryTree.scala
+++ src/main/scala/algorithm/BinaryTree.scala
@@ -7,11 +7,11 @@ import scala.util.Random
 class BinaryTree extends MazeSolver {
   private var _r = new Random()
 
-  def solve(grid: Grid): Vector[Vector[CellLink]] = {
+  override def solve(grid: Grid): Vector[Vector[CellLink]] = {
     val graph: Vector[Vector[CellLink]] = CellLink.createFrom(grid)
     for (c <- grid) {
       val curLink = graph(c.row)(c.col)
-      val cands = List(c.north, c.east).flatten
+      val cands: List[Cell] = List(c.north, c.east).flatten
       if (cands.nonEmpty) {
         val cand = if (cands.size > 1) cands(_r.nextInt(cands.size)) else cands(0)
         val otherLink = graph(cand.row)(cand.col)
--- src/test/scala/AlgoTest.scala
+++ src/test/scala/AlgoTest.scala
@@ -1,11 +1,20 @@
-import algorithm.{BinaryTree, CellLink}
+import algorithm.{BinaryTree, CellLink, Sidewinder}
 import grid.Grid
 import org.scalatest.FunSuite
 
 class AlgoTest extends FunSuite {
+  val grid = new Grid(8, 10)
+
   test("Algorithm.BinaryTree") {
-    val grid = new Grid(8, 10)
+
     val solver = new BinaryTree
+    val result = solver.solve(grid)
+    val s = CellLink.graphToString(result, grid)
+    print(s)
+  }
+
+  test("Algorithm.Sidewinder") {
+    val solver = new Sidewinder
     val result = solver.solve(grid)
     val s = CellLink.graphToString(result, grid)
     print(s)
