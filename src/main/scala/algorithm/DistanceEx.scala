bdd04c7f8f240faa2f3c3d44f43c2625140d13f3
Alvin Chien <alvinchien0624@gmail.com>
2020-05-08 20:35:13 -0400

    distanceMap

---
+++ src/main/scala/algorithm/DistanceEx.scala
@@ -0,0 +1,66 @@
+package algorithm
+
+import grid.{CellEx, GraphEx}
+
+import scala.collection.mutable
+
+//case class Position(val i: Int, val j: Int) { }
+
+trait DistanceEx {
+  def root: CellEx
+  def apply(cell: CellEx): Int
+  def contains(cell: CellEx)
+  def pathTo(position: CellEx): Option[List[CellEx]]
+}
+
+private class DistanceExImpl(val graph: GraphEx, val root: CellEx, val distMap: Map[CellEx, Int]) extends DistanceEx {
+  override def apply(cell: CellEx): Int = distMap(cell)
+
+  override def pathTo(position: CellEx): Option[List[CellEx]] = {
+    var path = List[CellEx]()
+    val que = mutable.Queue(position)
+    while (que.nonEmpty) {
+      val current = que.dequeue()
+      path = current +: path
+      if (current != root) {
+        val dist = this(current)
+        val candidates = graph.linkedCells(current) match {
+          case Some(cells) => cells.filter((p)=>this(p) == (dist-1)).toList
+          case None => List[CellEx]()
+        }
+        if (candidates.nonEmpty) que.enqueue(candidates.head)
+      } else {
+        que.clear()
+      }
+    }
+    if (path.head == root) Some(path) else None
+  }
+
+  override def contains(cell: CellEx): Unit = distMap.contains(cell)
+}
+
+object DistanceEx {
+  def from(graph: GraphEx, root: CellEx): Option[DistanceEx] = {
+    var distMap = Map(root -> 0)
+    val que = mutable.Queue(root)
+    while (que.nonEmpty) {
+      val size = que.size
+      for (_ <- 0 until size) {
+        val cell = que.dequeue()
+        val curDist = distMap(cell)
+        graph.linkedCells(cell) match {
+          case Some(cells) =>
+            cells.foreach((c: CellEx) => {
+              if (!distMap.contains(c)) {
+                distMap = distMap + (c -> (curDist+1))
+                que.enqueue(c)
+              }
+            })
+          case _ =>
+        }
+      }
+    }
+    if (distMap.nonEmpty) Some(new DistanceExImpl(graph, root, distMap))
+    else None
+  }
+}
--- src/test/scala/MazeGenTest.scala
+++ src/test/scala/MazeGenTest.scala
@@ -1,6 +1,6 @@
-import algorithm.{BinaryTreeMaze, SidewinderMaze}
+import algorithm.{BinaryTreeMaze, DistanceEx, SidewinderMaze}
 import com.sksamuel.scrimage.nio.PngWriter
-import grid.GridEx
+import grid.{CellEx, GridEx}
 import org.scalatest.FunSuite
 
 import scala.util.Random
@@ -21,6 +21,21 @@ class MazeGenTest extends FunSuite {
     val grid = new GridEx(8,10)
     val generator = new SidewinderMaze(rand)
     val maze = generator.generate(grid)
-    print(maze)
+    val distMap = DistanceEx.from(maze, grid(7, 0)).get
+    val content = (cell: CellEx) => {
+      val d = distMap(cell)
+      val sb = new StringBuilder()
+      sb.append(d)
+      while (sb.length() < 3) sb.append(' ')
+      sb.toString()
+    }
+    print(maze/*.dump(content)*/); println()
+    val goal = grid(7, 5)
+    val path = distMap.pathTo(goal)
+    if (path.isDefined) {
+      val pathSet = path.get.toSet;
+      print(maze.dump((c: CellEx) =>
+        if (pathSet.contains(c)) content(c) else " x " ))
+    }
   }
 }
