0bd11e4977767bd1fc3dda7d9aef95048a7dcbcc
Alvin Chien <alvinchien0624@gmail.com>
2020-08-09 16:06:41 -0400

    small change

--- src/main/scala/image/MazeImage.scala
+++ src/main/scala/image/MazeImage.scala
@@ -4,8 +4,12 @@ import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.color.RGBColor
 import maze.{Cell2D, Maze, Position2D}
 
+trait DrawFunc extends ( (Int, Int, Position2D => RGBColor) => ImmutableImage ) {
+  def apply(cellSize: Int, padding: Int, f: Position2D=>RGBColor): ImmutableImage
+}
+
 object MazeImage {
-  def drawFunc(maze: Maze[Cell2D]): (Int, Int, Position2D=>RGBColor)=>ImmutableImage = {
+  def drawFunc(maze: Maze[Cell2D]): DrawFunc = {
     def ret(cellSize: Int, padding: Int, f: Position2D=>RGBColor) = {
       val drawer = Drawer(maze, cellSize)
       val func: ImmutableImage=>ImmutableImage = {
