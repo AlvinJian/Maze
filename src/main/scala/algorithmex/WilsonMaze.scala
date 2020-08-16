f38746ef20359d8ee2db0a04e9628ad47a2c30ab
Alvin Chien <alvinchien0624@gmail.com>
2020-08-16 13:05:04 -0400

    refactor

--- src/main/scala/algorithmex/WilsonMaze.scala
+++ src/main/scala/algorithmex/WilsonMaze.scala
@@ -14,35 +14,34 @@ object WilsonMaze extends MazeGenerator {
       arr(random.nextInt(arr.length))
     }
     @tailrec
-    def roaming(maze: Maze[CC], unvisited: Set[Position2D],
-                pos: Position2D, prev: List[Position2D],
-                prevSet: Set[Position2D]): List[Position2D] = {
-      val neighbors = maze.neighborsAt(pos)
-      if (unvisited.contains(pos) && neighbors.nonEmpty) {
-        val p = neighbors(random.nextInt(neighbors.size)).pos
-        var set = prevSet; var path = prev
-        while (set.contains(p)) {
-          path match {
-            case ::(head, next) => {
-              path = next; set = set - head
-            }
-            case Nil =>
-          }
-        }
-        path = p +: path; set = set + p
-        roaming(maze, unvisited, p, path, set)
-      } else prev
-    }
-    @tailrec
     def loop(maze: Maze[CC], unvisited: Set[Position2D]): Maze[CC] = {
       if (unvisited.nonEmpty) {
         val pos = getUnvisitedOne(unvisited)
         val path = List(pos)
         val pathSet = path.toSet
-        val ret = roaming(maze, unvisited, pos, path, pathSet)
+        @tailrec
+        def roaming(pos: Position2D, prev: List[Position2D],
+                    prevSet: Set[Position2D]): List[Position2D] = {
+          val neighbors = maze.neighborsAt(pos)
+          if (unvisited.contains(pos) && neighbors.nonEmpty) {
+            val p = neighbors(random.nextInt(neighbors.size)).pos
+            var set = prevSet; var path = prev
+            while (set.contains(p)) {
+              set = path match {
+                case ::(head, next) => {
+                  path = next
+                  set - head
+                }
+                case Nil => set
+              }
+            }
+            roaming(p, p +: path, set + p)
+          } else prev
+        }
+        val ret = roaming(pos, path, pathSet)
         var newMaze = maze
         var newUnvisited = unvisited
-        val _ = ret.foldLeft[Option[Position2D]](None)(
+        val _ = ret.foldLeft[Option[Position2D]](None) {
           (prev, cur)=>{
             if (prev.isDefined) {
               newMaze = newMaze.link(prev.get, cur).get
@@ -50,7 +49,7 @@ object WilsonMaze extends MazeGenerator {
             }
             Some(cur)
           }
-        )
+        }
         loop(newMaze, newUnvisited)
       } else maze
     }
