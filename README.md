2db867be7b379f6a33a648479a65bf7de300fa19
Alvin Chien <alvinchien0624@gmail.com>
2020-09-27 11:19:33 -0400

    gif

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This is a pet project for me to learn and experiment Functional Programming and 
 |Recursive Backtrack|![polar maze](gallery/PolarMaze_RecurBackTrackMaze.png)|![polar maze path](gallery/PolarMaze_RecurBackTrackMaze_Path.png)|
 |Wilson|![weave maze](gallery/WeaveMaze_WilsonMaze.png)|![weave maze path](gallery/WeaveMaze_WilsonMaze_Path.png)|
 |Aldous Broder|![hex maze](gallery/HexMaze_AldousBroderMaze.png)|![hex maze path](gallery/HexMaze_AldousBroderMaze_Path.png)|
+
+Animation
+---
+![animated path](gallery/WeaveMaze_RecurBackTrackMaze_Path_Anim.gif)
--- src/main/scala/image/GifCreator.scala
+++ src/main/scala/image/GifCreator.scala
@@ -9,17 +9,19 @@ import com.sksamuel.scrimage.nio.StreamingGifWriter
 import scala.concurrent._
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 
+// TODO the API is not good enough
 class GifCreator[T](input: Seq[T], f: T => ImmutableImage) {
   private val futureImages: Seq[Future[ImmutableImage]] = input.map(t => Future(f(t)))
 
-  def produce(duration: Long, target: File, infLoop: Boolean = true): Try[Unit] = {
+  def produce(duration: Long, target: File, infLoop: Boolean): Try[Unit] = {
     val writer = new StreamingGifWriter(java.time.Duration.ofSeconds(duration), infLoop)
     val stream = writer.prepareStream(target, BufferedImage.TYPE_INT_ARGB)
     val res = futureImages.foldLeft(Try(stream))((prevStream, futureImage) => {
       val image = Await.result(futureImage, 60 seconds)
-      prevStream.flatMap(s => Try(s.writeFrame(image)))
+      prevStream.flatMap(s => Try(s.writeFrame(image, java.time.Duration.ofMillis(100))))
     })
     res.map(s => s.close())
   }
